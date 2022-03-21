b029016716db8e143f6beba961428d231d28005b
andreikurnosov <nixdie@yandex.ru>
2022-03-21 18:51:29 +0700

    Added gsap animation to cookie popup

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+const tl = gsap.timeline({defaults: {duration: 0.75, ease: 'power1.out'}})
+
+tl.fromTo('.cookie_container', {scale: 0}, {scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5})
+tl.fromTo('.cookie', {opacity: 0, x: -50, rotation: '-45deg'}, {opacity: 1, x: 0, rotation: '0deg'},'<50%')
+tl.fromTo('.text', {x:30, opacity: 0}, {x: 0, opacity: 1}, '<')
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  gsap.to('.cookie_container', {opacity: 0, y: 100, duration: 0.75, ease: 'power1.out'})
+})
+
